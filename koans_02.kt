39cd94f2045472ee8477b6d3f68ee1880cdde3b0
Taison Tsukada <78004609+TaisonTsukada@users.noreply.github.com>
2022-06-19 14:53:03 +0900

    add: question

--- koans_02.kt
+++ koans_02.kt
@@ -1,2 +1,7 @@
+// Named arguments
+// Make the function joinOptions() return the list in a JSON format (for example, [a, b, c]) by specifying only two arguments.
+// Default and named arguments help to minimize the number of overloads and improve the readability of the function invocation. The library function joinToString is declared with default values for parameters:
+
+
 fun joinOptions(options: Collection<String>) =
         options.joinToString(prefix = "[", postfix= "]")
